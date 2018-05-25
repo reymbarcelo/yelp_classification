8237cb2c9388463b48e88ef135e1a385ed59d7af
rey <rbarcelo@stanford.edu>
2018-05-25 14:18:10 -0700

    Add files for evaluation of SGDModel and SVCModel.

--- models.py
+++ models.py
@@ -1,10 +1,11 @@
 # TODO: try out different models!
 
-import tensorflow as tf
+# import tensorflow as tf
 
 from sklearn.linear_model import SGDClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
 from sklearn.svm import LinearSVC
 
 class ExampleModel():
@@ -58,25 +59,55 @@ class NeighborsModel():
 	def predict(self, X_test):
 		return self.model.predict(X_test)
 
-########################TENSORFLOW#MODELS########################
-
-# TODO: fix this
-class TFModel(ExampleModel):
+class NeuralModel():
 	def __init__(self):
-		self.model = tf.estimator.DNNClassifier(
-			hidden_units=[256, 32],
-			feature_columns=[tf.feature_column.numeric_column('x', shape=[28, 28])]
-		)
+		self.model = MLPClassifier()
 
 	def fit(self, X_train, Y_train):
-		self.model.train(X_train)
+		self.model.fit(X_train, Y_train)
 
 	def predict(self, X_test):
 		return self.model.predict(X_test)
 
+########################TENSORFLOW#MODELS########################
 
+# TODO: fix this
+# class TFModel(ExampleModel):
+# 	def __init__(self):
+# 		self.model = tf.estimator.DNNClassifier(
+# 			hidden_units=[256, 32],
+# 			feature_columns=[tf.feature_column.numeric_column('x', shape=[28, 28])]
+# 		)
 
+# 	def fit(self, X_train, Y_train):
+# 		self.model.train(X_train)
 
+# 	def predict(self, X_test):
+# 		return self.model.predict(X_test)
+
+########################PYTORCH#MODELS########################
+
+# TODO: finish this
+# class TorchModel(ExampleModel):
+# 	def __init__(self):
+# 		self.model = torch.nn.Sequential(
+# 			torch.nn.Linear(input_num_units, hidden_num_units),
+# 			torch.nn.ReLU(),
+# 			torch.nn.Linear(hidden_num_units, output_num_units),
+# 		)
+# 		self.loss_fn = torch.nn.CrossEntropyLoss()
+# 		self.optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+# 		self.epochs = 5
+
+# 	def fit(self, X_train, Y_train):
+# 		return
+
+# 	# Params:
+# 	# 	X_test: 2d feature vector
+# 	# Returns: 
+# 	#	Y_test: 1d label vector
+# 	def predict(self, X_test):
+# 		return []
 
 
 
