fc6c2b5f0b676088ff71aa9f29780ea30dc9cd9a
rey <rbarcelo@stanford.edu>
2018-05-25 15:47:32 -0700

    Add star rating to features.

--- features.py
+++ features.py
@@ -4,23 +4,21 @@ from collections import defaultdict
 
 def featurize(review):
 	# TODO: make this cleaner
-	featurized_review = {}
-	for k, v in bag_of_words(review).items():
-		featurized_review[k] = v
-	for k, v in bigrams(review).items():
-		featurized_review[k] = v
+	featurized_review = defaultdict(int)
+	bag_of_words(featurized_review, review)
+	bigrams(featurized_review, review)
+	stars(featurized_review, review)
 	return featurized_review
 
-def bag_of_words(review):
-	featurized_review = defaultdict(int)
+def bag_of_words(featurized_review, review):
 	for word in review['text'].lower().split():
 		featurized_review[word] += 1
-	return featurized_review
 
-def bigrams(review):
-	featurized_review = defaultdict(int)
+def bigrams(featurized_review, review):
 	split_review = review['text'].lower().split()
 	for i in range(len(split_review) - 1):
 		featurized_review[split_review[i] + '_' + split_review[i+1]] += 1
-	return featurized_review
+
+def stars(featurized_review, review):
+	featurized_review['FEATURE_STARS'] == review['stars']
 
