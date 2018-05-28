2766e3ee627ad715a5e30a7c80b628ca1771b7d3
rey <rbarcelo@stanford.edu>
2018-05-28 10:42:09 -0700

    - Porter stemming and POS tagging in features.py - New eval files in eval/

--- features.py
+++ features.py
@@ -1,12 +1,16 @@
 # TODO: add lots more of these!
+import nltk
 
 from collections import defaultdict
+from nltk.stem import *
 
 def featurize(review):
 	featurized_review = defaultdict(int)
 	bag_of_words(featurized_review, review)
 	bigrams(featurized_review, review)
 	stars(featurized_review, review)
+	stem(featurized_review, review)
+	pos_tag(featurized_review, review)
 	return featurized_review
 
 def bag_of_words(featurized_review, review):
@@ -21,3 +25,11 @@ def bigrams(featurized_review, review):
 def stars(featurized_review, review):
 	featurized_review['FEATURE_STARS'] == review['stars']
 
+def stem(featurized_review, review):
+	stemmer = PorterStemmer()
+	for word in review['text'].lower().split():
+		featurized_review[stemmer.stem(word)] += 1
+
+def pos_tag(featurized_review, review):
+	for word, tag in nltk.pos_tag(nltk.word_tokenize(review['text'])):
+		featurized_review[word + '_' + tag] += 1
