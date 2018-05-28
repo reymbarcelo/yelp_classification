ed197844eb8c58a80e0cfdd992206c1b09e9dc10
rey <rbarcelo@stanford.edu>
2018-05-28 11:07:54 -0700

    Fix bug in stars()

--- features.py
+++ features.py
@@ -2,15 +2,17 @@
 import nltk
 
 from collections import defaultdict
+from nltk.corpus import stopwords
 from nltk.stem import *
 
 def featurize(review):
 	featurized_review = defaultdict(int)
-	bag_of_words(featurized_review, review)
-	bigrams(featurized_review, review)
+	# bag_of_words(featurized_review, review)
+	# bigrams(featurized_review, review)
 	stars(featurized_review, review)
-	stem(featurized_review, review)
-	pos_tag(featurized_review, review)
+	# stem(featurized_review, review)
+	# pos_tag(featurized_review, review)
+	preprocessed_bag_of_words(featurized_review, review)
 	return featurized_review
 
 def bag_of_words(featurized_review, review):
@@ -23,7 +25,7 @@ def bigrams(featurized_review, review):
 		featurized_review[split_review[i] + '_' + split_review[i+1]] += 1
 
 def stars(featurized_review, review):
-	featurized_review['FEATURE_STARS'] == review['stars']
+	featurized_review['FEATURE_STARS'] = review['stars']
 
 def stem(featurized_review, review):
 	stemmer = PorterStemmer()
@@ -33,3 +35,33 @@ def stem(featurized_review, review):
 def pos_tag(featurized_review, review):
 	for word, tag in nltk.pos_tag(nltk.word_tokenize(review['text'])):
 		featurized_review[word + '_' + tag] += 1
+
+def preprocessed_bag_of_words(featurized_review, review):
+	stemmer = PorterStemmer()
+	stop = set(stopwords.words('english'))
+	for word, tag in nltk.pos_tag(nltk.word_tokenize(review['text'])):
+		if word in stop:
+			continue
+		featurized_review[stemmer.stem(word) + '_' + tag] += 1	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
